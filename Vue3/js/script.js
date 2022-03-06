7f8c300510d71c58f6389a6ab587cbea12479178
kiiii8r <gkii.8r@gmail.com>
2022-03-06 19:59:16 +0900

    No blank input & list view

--- Vue3/js/script.js
+++ Vue3/js/script.js
@@ -6,12 +6,16 @@ const app = Vue.createApp({
   methods: {
     // クリック時のイベントを設定
     addItem: function(event) {
-    console.log('Clicked!')
+
+    // 空入力は処理を終了する
+    if(this.newItem === '') return
+
     // todosにnewItemの記載内容を格納する
     let todo = {
       item: this.newItem
     }
     this.todos.push(todo)
+    
     // Addボタンクリック時、入力欄をリセットする
     this.newItem = ''
     }
