58335525403457c45c885a393d4fb31d61ce30b3
kiiii8r <gkii.8r@gmail.com>
2022-03-06 19:49:40 +0900

    Stored in array

--- Vue3/js/script.js
+++ Vue3/js/script.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
   data: () => ({
-    newItem: ''
+    newItem: '',
+    todos: []
   }),
   methods: {
     // クリック時のイベントを設定
     addItem: function(event) {
     console.log('Clicked!')
+    // todosにnewItemの記載内容を格納する
+    let todo = {
+      item: this.newItem
+    }
+    this.todos.push(todo)
     }
   }
 }).mount('#app')
